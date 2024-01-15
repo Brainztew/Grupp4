263995fddec5ba7c6dcc56c7d18e564b6f7da003
Brainztew <isacsson_89@hotmail.com>
2024-01-15 15:25:08 +0100

    woho

---
+++ script.js
@@ -0,0 +1,56 @@
+
+let movieList = document.getElementById("movieList");
+let movieInfo = document.getElementById("movieInfo");
+
+console.log("hej från js");
+
+function fetchMovies(url) {
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => printMovieList(data.results));
+}
+
+function searchMovies() {
+    let searchTerm = document.getElementById("searchInput").value;
+    let searchUrl = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${searchTerm}&api_key=88d6f906b386ac47c004701d8f545df8`;
+ 
+    fetchMovies(searchUrl);
+    let headLineSearch = document.createElement("h2");
+    headLineSearch.innerText = "Sökresultat";
+    movieList.appendChild(headLineSearch)
+}
+
+function printMovieList(movies) {
+    movieList.innerHTML = "";
+
+    movies.forEach(movie => {
+        let li = document.createElement("li");
+        li.innerText = movie.original_title;
+
+        li.addEventListener("click", () => {
+            printMovieInfo(movie);
+        });
+
+        movieList.appendChild(li);
+    });
+}
+
+function printMovieInfo(movie) {
+    movieInfo.innerHTML = "";
+
+    let movieDiv = document.createElement("div");
+    let movieHeadline = document.createElement("h2");
+    movieHeadline.innerText = movie.original_title;
+
+    let movieText = document.createElement("p");
+    movieText.innerText = movie.overview;
+
+    let movieImg = document.createElement("img");
+    movieImg.style.width = "500px";
+    movieImg.src = "https://image.tmdb.org/t/p/original/" + movie.poster_path;
+
+    movieDiv.append(movieHeadline, movieText, movieImg);
+    movieInfo.appendChild(movieDiv);
+}
+
+fetchMovies("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=88d6f906b386ac47c004701d8f545df8");
